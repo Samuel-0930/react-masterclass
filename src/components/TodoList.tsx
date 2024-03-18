286ef64bf5bb4a7628bccded925c9da7f5523a08
Samyeol Son <samyeol0930@gmail.com>
2024-03-18 21:39:29 +0900

    [#6.19] Recap

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,65 +1,39 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import { atom, useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { Categories, categoryState, toDoSelector } from '../atoms';
+import CreateToDo from './CreateToDo';
+import ToDo from './ToDo';
 
 // ToDoList.tsx
 type Props = {
 	// props의 타입 정의
 };
 
-interface IForm {
-	toDo: string;
-	// 다른 필드들도 추가할 수 있습니다.
-}
-
-interface IToDo {
-	text: string;
-	id: number;
-	category: 'TO_DO' | 'DOING' | 'DONE';
-}
-
-const toDoState = atom<IToDo[]>({
-	key: 'toDo',
-	default: [],
-});
-
 const ToDoList: React.FC<Props> = () => {
-	const [toDos, setToDos] = useRecoilState(toDoState);
-	const {
-		register,
-		handleSubmit,
-
-		setValue,
-	} = useForm<IForm>();
-	const handleValid = ({ toDo }: IForm) => {
-		console.log('add to do', toDo);
-		setToDos((oldToDos) => [
-			{ text: toDo, id: Date.now(), category: 'TO_DO' },
-			...oldToDos,
-		]);
-		setValue('toDo', '');
+	const toDos = useRecoilValue(toDoSelector);
+	const [category, setCategory] = useRecoilState(categoryState);
+	const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+		setCategory(event.currentTarget.value as any);
 	};
-	console.log(toDos);
 
 	return (
 		<div>
 			<h1>To Dos</h1>
 			<hr />
-			<form onSubmit={handleSubmit(handleValid)}>
-				<input
-					{...register('toDo', {
-						required: 'Please write a To Do',
-					})}
-					placeholder='Write a to do'
+			<select
+				value={category}
+				onInput={onInput}>
+				<option value={Categories.TO_DO}>To Do</option>
+				<option value={Categories.DOING}>Doing</option>
+				<option value={Categories.DONE}>Done</option>
+			</select>
+			<CreateToDo />
+			{toDos?.map((toDo) => (
+				<ToDo
+					key={toDo.id}
+					{...toDo}
 				/>
-
-				<button>Add</button>
-			</form>
-			<ul>
-				{toDos.map((toDo) => (
-					<li key={toDo.id}>{toDo.text}</li>
-				))}
-			</ul>
+			))}
 		</div>
 	);
 };
